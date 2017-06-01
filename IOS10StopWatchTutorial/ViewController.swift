1a46f2d136e747bf1a81e7da1f1eb88ffe9d998e
Michelle Bergquist <caker25@gmail.com>
2017-06-01 11:44:00 -0600

    Stop watch

--- IOS10StopWatchTutorial/ViewController.swift
+++ IOS10StopWatchTutorial/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0.0
+    var timer = Timer()
+    var isPlaying = false
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBAction func startTimer(_ sender: Any) {
+        if(isPlaying) {
+            return
+        }
+        startButton.isEnabled = false
+        pauseButton.isEnabled = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.UpdateTimer), userInfo: nil, repeats: true)
+        
+        isPlaying = true
+    }
+    @IBAction func pauseTimer(_ sender: Any) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+        counter = 0.0
+        timeLabel.text = String(counter)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timeLabel.text = String(counter)
+        pauseButton.isEnabled = false
     }
-
+    
+    func UpdateTimer() {
+        counter += 0.1
+        timeLabel.text = String(format: "%.1f", counter)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
