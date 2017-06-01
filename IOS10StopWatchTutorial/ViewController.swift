0fd4ace504ed5c7a33647211556cba94f3916b23
Michelle Bergquist <caker25@gmail.com>
2017-06-01 10:47:27 -0600

    Initial Commit

---
+++ IOS10StopWatchTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS10StopWatchTutorial
+//
+//  Created by Michelle Bergquist on 6/1/17.
+//  Copyright © 2017 Michelle Bergquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
